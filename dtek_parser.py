a885ba622483e7202ec51e408f8d5998154bfe45
Semstep <semstep2001@gmail.com>
2023-02-03 14:32:39 +0200

    Проба сеарилизовать суп. RecursionError, но пусть побудет для истории. Плюс ручное логгирование кук из браузера. Но преттифай сериализуется

--- dtek_parser.py
+++ dtek_parser.py
@@ -4,6 +4,7 @@ https://stackoverflow.com/questions/74743249/cant-access-site-programmatically
 import requests
 import urllib.parse
 from bs4 import BeautifulSoup as bs
+from get_dtek_cookie import showdic
 from dtek_pickle_table import set_cookie
 
 resp_err_text = 'Request unsuccessful. Incapsula incident ID: 689000950002755466-6069776676886470'
@@ -15,7 +16,7 @@ s = requests.Session()
 # s.cookies[r'incap_ses_689_2224657'] = r'g3yiYL2nzBOgRgSrttKPCc4A3GMAAAAAFyONVoVXdmQMv/hevwLYdA=='
 # # s.cookies['visid_incap_2224657'] = '0U/cw1+IS1+3wORv6R5za5r922MAAAAAQUIPAAAAAADie8Y8jyxSMdOSW2ci9ZQm'
 # headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-set_cookie(s, rawstr=r'incap_ses_689_2224657=vfTqI6LKnEjARQirttKPCS0W3GMAAAAA6C83W9Ll4txHMj4dBJ61fA==')
+set_cookie(s, rawstr=r'incap_ses_763_2224657=LN4QCM9/sGWur1RZPbmWCtfG02MAAAAAY7Zf0vCukCz3wzNrFNDvAA==')
 resp = s.get(urllib.parse.urljoin(dtek_url, '/ua/shutdowns'))
 print(resp.status_code)
 soup = bs(resp.content, 'lxml')
